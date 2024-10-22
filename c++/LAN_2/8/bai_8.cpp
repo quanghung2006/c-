77ecb2db940f9b8b18f62379b459bd0879f9e829
quanghung2006 <hungsenpai2006@gmail.com>
2024-10-22 22:33:53 +0700

    chaunopbai

--- c++/LAN_2/8/bai_8.cpp
+++ c++/LAN_2/8/bai_8.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+using namespace std ;
 
-int main() {
-    std::cout << "Hello, World 1234567!" << std::endl;
-    return 0;
+ int main(){
+// khai báo//
+ int doanhthu;
+float hoahong;
+//in man hình //
+cout<< "nhap doanh thu ban hang : ";
+cin>> doanhthu;
+//so sánh//
+ if(doanhthu<=100){
+hoahong=doanhthu*5/100 ;
+cout <<"tong doanh thu la:"<<doanhthu<< endl;
+cout << "hoa hong nhan duoc la :"<<hoahong<< endl ;
+
+}
+else if(doanhthu<=300);{	
+hoahong=doanhthu *10/100;
+cout <<"tong doanh thu la :"<<doanhthu<< endl ;
+cout << "hoa hong nhan duoc la :"<<hoahong<<endl ;
+}
+ if (doanhthu > 300);{
+hoahong=doanhthu* 20/100 ;
+cout <<"tong doanh thu la:"<<doanhthu<<endl ;
+cout << "hoa hong nhan duoc la :"<<hoahong<<endl ;
+}
+return 0;
 }
