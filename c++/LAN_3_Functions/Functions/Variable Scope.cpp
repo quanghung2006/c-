35e649d07b6dd139e3a096fa1140640584ddca08
quanghung2006 <hungsenpai2006@gmail.com>
2024-11-13 23:08:06 +0700

    function

---
+++ c++/LAN_3_Functions/Functions/Variable Scope.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+  
+  
+void  myFunction() {
+ int x = 5 ;
+ float  y = 10 ;
+ cout << x << "\n";
+ cout << y;
+}
+int main() 	{
+
+ myFunction();
+return 0;
+}
